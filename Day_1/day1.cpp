bba0ef11a2f65219fbb480ac2fe4174a4295c9c3
Akash Sarode <akashtechnion@gmail.com>
2025-12-22 11:47:42 -0500

    Completed day 1

---
+++ Day_1/day1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    std::ifstream inputFile("input.txt");
+
+    if (!inputFile.is_open()) {
+        std::cerr << "File can't open" << std::endl;
+        return 1;
+    }
+
+    std::string line;
+    int currVal = 50;
+    int code = 0;
+
+    while (std::getline(inputFile, line)) {
+        std::string dir = line.substr(0, 1);
+        int diff = std::stoi(line.substr(1));
+
+        if (dir == "L") {
+            currVal = (currVal - diff + 100) % 100;
+        } else {
+            currVal = (currVal + diff) % 100;
+        }
+
+        if (currVal == 0) {
+            code++;
+        }
+    }
+
+    std::cout << code << std::endl;
+
+    return 0;
+}
